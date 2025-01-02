32f4f6ff41d3cc5c7c99976eca74d3c90e398cdc
ManthanBR <155616252+ManthanBR@users.noreply.github.com>
2025-01-02 20:58:45 +0530

    second

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "DefaultCompany-UrbanHarvest-0.1";
 const contentToCache = [
-    "Build/git.loader.js",
-    "Build/git.framework.js",
-    "Build/git.data",
-    "Build/git.wasm",
+    "Build/UrbanHarvest.loader.js",
+    "Build/UrbanHarvest.framework.js",
+    "Build/UrbanHarvest.data",
+    "Build/UrbanHarvest.wasm",
     "TemplateData/style.css"
 
 ];
